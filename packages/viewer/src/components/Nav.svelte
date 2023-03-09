033b8b4ca8aaad1b9d17ce896f5e4c7d216d0683
Steven Clontz <steven.clontz@gmail.com>
2023-03-09 05:37:49 +0000

    add advanced link when onMain branch

--- packages/viewer/src/components/Nav.svelte
+++ packages/viewer/src/components/Nav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import context from '../context'
   import { contributingUrl, mainBranch } from '../constants'
+  import Branch from './Shared/Icons/Branch.svelte';
 
   import Link from './Nav/Link.svelte'
 
@@ -21,9 +22,13 @@
     </div>
 
     <div class="navbar-nav">
-      {#if showDev || !onMain}
-        <Link to="/dev">{$source.branch}</Link>
-      {/if}
+      <Link to="/dev">
+        {#if showDev || !onMain}
+          <Branch/> {$source.branch}
+        {:else}
+          Advanced
+        {/if}
+      </Link>
       <a class="nav-link" href={contributingUrl}>Contribute</a>
     </div>
   </div>
