f84345399508b23e1bce61b20062ef53867310e6
Steven Clontz <steven.clontz@gmail.com>
2023-05-12 18:21:54 +0000

    Clarify copyright

--- packages/viewer/src/components/Footer.svelte
+++ packages/viewer/src/components/Footer.svelte
@@ -1,9 +1,10 @@
 <!-- Footer -->
 <footer class="text-center bg-light text-muted">
   <div class="p-4">
-    © 2014-{(new Date()).getFullYear()} James Dabbs |
-    <a class="text-dark" href="https://github.com/pi-base">GitHub</a> |
-    <a class="text-dark" href="https://github.com/pi-base/data/blob/main/LICENSE.md">CC-BY</a> |
-    <a class="text-dark" href="https://github.com/pi-base/web/blob/main/LICENSE.md">MIT License</a>
+    Data © {(new Date()).getFullYear()} Steven Clontz and James Dabbs
+    (<a class="text-dark" href="https://github.com/pi-base/data/blob/main/LICENSE.md">CC-BY</a>) |
+    Software © {(new Date()).getFullYear()} James Dabbs
+    (<a class="text-dark" href="https://github.com/pi-base/web/blob/main/LICENSE.md">MIT License</a>) |
+    <a class="text-dark" href="https://github.com/pi-base">GitHub</a>
   </div>
 </footer>
