15078207abc1a511583b61de978cd5ea8214d20c
James Dabbs <james.dabbs@gmail.com>
2023-05-14 14:54:50 -0600

    ci: set up cypress

---
+++ packages/viewer/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: '7u7evp',
+  chromeWebSecurity: false,
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.{ts,tsx}',
+    supportFile: false,
+  },
+})
