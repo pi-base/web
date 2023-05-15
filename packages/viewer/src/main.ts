173784fb0cdd0043b2ec0084b9086cc46400dcd1
James Dabbs <james.dabbs@gmail.com>
2023-05-15 11:32:07 -0600

    feat: support sentry reports from Cloudflare pages

--- packages/viewer/src/main.ts
+++ packages/viewer/src/main.ts
@@ -1,8 +1,13 @@
 import App from './components/App.svelte'
 import * as errors from './errors'
 
-const dev =
-  window.location.host.match(/(dev(elopment)?[.-]|localhost)/) !== null
+const host = window.location.host
+const dev = host.match(/(dev(elopment)?[.-]|localhost)/) !== null
+const errorEnv = ['topology.pi-base.org', 'topology.pages.dev'].includes(host)
+  ? 'production'
+  : host.includes('pages.dev')
+  ? 'deploy-preview'
+  : 'dev'
 
 const app = new App({
   target: document.body,
@@ -12,6 +17,7 @@ const app = new App({
       ? errors.log()
       : errors.sentry(
           'https://0fa430dd1dc347e2a82c413d8e3acb75@o397472.ingest.sentry.io/5251960',
+          errorEnv,
         ),
   },
 })
