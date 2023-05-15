278dd99dc1e8c231d63c89b4b86842d6d37b5438
Steven Clontz <steven.clontz@gmail.com>
2023-05-15 21:31:54 +0000

    add markdown copy button

---
+++ packages/viewer/src/components/Shared/CopyButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let text = ""
+    let confirmIcon = false
+    const click = (_:Event) => {
+        navigator.clipboard.writeText(text)
+        confirmIcon = true
+        setTimeout(() => confirmIcon = false, 3000)
+    }
+</script>
+
+<button type="button" class="btn btn-outline-secondary btn-sm" on:click={click} title={text}>
+    {#if confirmIcon}Copied! ✓{:else}<slot/>{/if}
+</button>
