3e15c371ace21564b9a37cd858623e44bd29301d
Steven Clontz <steven.clontz@gmail.com>
2023-11-25 17:21:08 +0000

    fix trim tests, add tag tests

--- packages/core/test/Id.test.ts
+++ packages/core/test/Id.test.ts
@@ -31,18 +31,44 @@ describe('toInt', () => {
 
 describe('trim', () => {
   it('trims down to the numeric content', () => {
-    expect(trim('T000100')).toEqual(100)
+    expect(trim('T000100')).toEqual("100")
   })
 
   it('does not require leading zeros', () => {
-    expect(trim('P12')).toEqual(12)
+    expect(trim('P12')).toEqual("12")
   })
 
-  it('does not match nonexistent kinds', () => {
-    expect(trim('Z000100')).toEqual(0)
+  it('returns self for nonexistent kinds', () => {
+    expect(trim('Z000100')).toEqual("Z000100")
   })
 
   it('matches unknown kind', () => {
-    expect(trim('000123')).toEqual(123)
+    expect(trim('000123')).toEqual("123")
+  })
+})
+
+describe('tag', () => {
+  it('tags spaces', () => {
+    expect(tag('S000100')?.kind).toEqual("space")
+  })
+
+  it('tags properties', () => {
+    expect(tag('P000100')?.kind).toEqual("property")
+  })
+
+  it('tags theorems', () => {
+    expect(tag('T000100')?.kind).toEqual("theorem")
+  })
+
+  it('tags unknowns', () => {
+    expect(tag('000100')?.kind).toEqual("unknown")
+  })
+
+  it('is null for nonexistent kinds', () => {
+    expect(tag('X000100')).toEqual(null)
+  })
+
+  it('allows lowercase', () => {
+    expect(tag('s000100')?.kind).toEqual("space")
   })
 })
