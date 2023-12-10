d9fc44a189e8fcee53baf23762314eb4814e2079
James Dabbs <james.dabbs@gmail.com>
2023-12-10 11:24:17 -0800

    fix trait#show page reactivity (#94)
    per report [here](https://github.com/orgs/pi-base/discussions/464#discussioncomment-7560692)

--- packages/viewer/src/routes/(app)/spaces/[id]/properties/[propertyId]/+page.svelte
+++ packages/viewer/src/routes/(app)/spaces/[id]/properties/[propertyId]/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { Show } from '@/components/Traits'
+  import { Icons, Link, References, Title, Typeset } from '@/components/Shared'
+  import { Proof } from '@/components/Traits'
+
+  import type { PageData } from './$types'
+  export let data: PageData
 </script>
 
-<Show spaceId={$page.params.id} propertyId={$page.params.propertyId} />
+<Title title={`${data.space.name}: ${data.property.name}`} />
+
+<h1>
+  {#if data.proof}
+    <Icons.Robot />
+  {/if}
+  <Link.Space space={data.space} />
+  is
+  {data.trait.value ? '' : 'not'}
+  <Link.Property property={data.property} />
+</h1>
+
+{#if data.proof}
+  <Proof space={data.space} {...data.proof} />
+{:else if data.meta}
+  <section class="description">
+    <Typeset body={data.meta.description} />
+  </section>
+  <h3>References</h3>
+  <References references={data.meta.refs} />
+{/if}
