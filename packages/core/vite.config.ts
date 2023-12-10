12b5fd5b624b9bd02b28658b30bef339e053f9c4
James Dabbs <james.dabbs@gmail.com>
2023-12-10 14:12:55 -0800

    truncate previews at first line break (#96)

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -17,10 +17,10 @@ export default defineConfig({
   },
   test: {
     coverage: {
-      lines: 91.04,
-      branches: 93.68,
-      statements: 91.04,
-      functions: 83.55,
+      lines: 93.04,
+      branches: 94.35,
+      statements: 93.04,
+      functions: 84.21,
       skipFull: true,
       thresholdAutoUpdate: true,
       exclude: ['src/Formula/Grammar.ts', 'test'],
