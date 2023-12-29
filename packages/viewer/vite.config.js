e616e54c78d3939e24731ad0cd65dbe3b1e46f77
James Dabbs <james.dabbs@gmail.com>
2023-12-29 08:47:36 -0500

    feat: proxying to local bundle server in dev

--- packages/viewer/vite.config.js
+++ packages/viewer/vite.config.js
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
+const compileServer = process.env.COMPILE_SERVER_URL || 'http://localhost:3141'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/bundle': {
+        target: compileServer,
+        rewrite: path => path.replace(/^\/bundle/, ''),
+      },
+    },
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
     coverage: {
