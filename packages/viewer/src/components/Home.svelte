96a45da765a90bb5c76a9db0f53220f9ee98a3ca
Steven Clontz <steven.clontz@gmail.com>
2024-03-23 15:26:33 -0500

    change example to T40 based on community call

--- packages/viewer/src/components/Home.svelte
+++ packages/viewer/src/components/Home.svelte
@@ -24,8 +24,8 @@
       </p>
       <p class="mb-0">
         ➜ Find counterexamples:
-        <a href="/theorems/T000021" class="text-info"
-          >spaces with the countable chain condition need not be separable</a
+        <a href="/theorems/T000040" class="text-info"
+          >connected spaces need not be path connected</a
         >
       </p>
     </div>
