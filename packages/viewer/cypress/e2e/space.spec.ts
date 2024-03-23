584e8b2a41e4d478b4e2d7481d1a825e105b4ba1
Steven Clontz <steven.clontz@gmail.com>
2024-03-23 20:39:23 +0000

    add broken e2e test for references link

--- packages/viewer/cypress/e2e/space.spec.ts
+++ packages/viewer/cypress/e2e/space.spec.ts
@@ -43,3 +43,13 @@ it('displays trait descriptions', () => {
 
   cy.contains('All subsets of this space are open by definition.')
 })
+
+it('displays references', () => {
+  cy.visit('spaces/S000001')
+
+  cy.get('.nav-link')
+    .contains("References")
+    .click()
+
+  cy.contains('Discrete Space on Wikipedia.')
+})
