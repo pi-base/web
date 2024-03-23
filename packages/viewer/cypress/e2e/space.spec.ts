21b956a274f65cd94b7a1b4efd493eed3d43432d
Steven Clontz <steven.clontz@gmail.com>
2024-03-23 20:40:21 +0000

    reformat

--- packages/viewer/cypress/e2e/space.spec.ts
+++ packages/viewer/cypress/e2e/space.spec.ts
@@ -47,9 +47,7 @@ it('displays trait descriptions', () => {
 it('displays references', () => {
   cy.visit('spaces/S000001')
 
-  cy.get('.nav-link')
-    .contains("References")
-    .click()
+  cy.get('.nav-link').contains('References').click()
 
   cy.contains('Discrete Space on Wikipedia.')
 })
