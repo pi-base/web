11a52e691d30d01d71d72c66d251d8ad17ff5738
Steven Clontz <steven.clontz@gmail.com>
2024-04-11 20:52:16 -0500

    add contributing link for missing references (#145)
    * add contributing link for missing references
    
    * lint

--- packages/viewer/src/components/Shared/References.svelte
+++ packages/viewer/src/components/Shared/References.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { Ref } from '@pi-base/core'
   import Reference from './Reference.svelte'
+  import { contributingUrl } from '@/constants'
 
   export let references: Ref.Ref[]
 </script>
@@ -9,6 +10,12 @@
   {#each references as reference, i (i)}
     <li>
       <Reference {reference} />
+    </li>
+  {:else}
+    <li>
+      No references available. If appropriate, please consider <a
+        href={contributingUrl}>contributing</a
+      > a reference.
     </li>
   {/each}
 </ul>
