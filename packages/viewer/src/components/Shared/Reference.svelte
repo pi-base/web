390b3e857a2c555bdf25635b57d8f336773d7750
Steven Clontz <steven.clontz@gmail.com>
2024-05-13 21:54:09 -0500

    typeset reference (#153)

--- packages/viewer/src/components/Shared/Reference.svelte
+++ packages/viewer/src/components/Shared/Reference.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Ref } from '@pi-base/core'
   import { format } from '@/parser/externalLinks'
+  import { Typeset } from '../Shared'
 
   export let reference: Ref.Ref
 
@@ -8,5 +9,5 @@
 </script>
 
 {#if value}
-  <a href={value.href}>{value.title}</a>
+  <a href={value.href}><Typeset body={value.title} /></a>
 {:else}Invalid reference: <code>{JSON.stringify(reference)}</code>{/if}
