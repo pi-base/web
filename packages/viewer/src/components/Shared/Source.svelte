13ddaf78f4512a2b2bd8902991b83e12131d634e
Steven Clontz <steven.clontz@gmail.com>
2024-09-19 04:08:04 +0000

    add button to show markdown source of files

---
+++ packages/viewer/src/components/Shared/Source.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let source: string
+  let showMarkdown = false
+</script>
+
+<button
+  class="btn btn-secondary btn-sm mb-3"
+  on:click={_ => (showMarkdown = !showMarkdown)}
+>
+  {#if showMarkdown}Hide{:else}Show{/if} markdown
+</button>
+{#if showMarkdown}
+  <pre>{source}</pre>
+{/if}
