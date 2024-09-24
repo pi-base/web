a2ff68309f2fb706f7cc571f371d675d68e45ce1
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-09-24 21:21:24 +0000

    lint

--- packages/viewer/src/components/Shared/Cite.svelte
+++ packages/viewer/src/components/Shared/Cite.svelte
@@ -12,21 +12,21 @@
 </script>
 
 {#key title}
-<div class="text-center p-2">
-  <small>
-    Cite as:
-    <span class="text-muted">
-      The pi-Base Community.
-      <cite>{title}.</cite>
-      Available at: {url}
-      (Accessed: {new Date().toISOString().split('T')[0]}).
-    </span>
-  </small>
-</div>
-<div class="text-center p-2">
-  <small>
-    Copy:
-    <CopyButton text={markdown}>Markdown Link</CopyButton>
-  </small>
-</div>
+  <div class="text-center p-2">
+    <small>
+      Cite as:
+      <span class="text-muted">
+        The pi-Base Community.
+        <cite>{title}.</cite>
+        Available at: {url}
+        (Accessed: {new Date().toISOString().split('T')[0]}).
+      </span>
+    </small>
+  </div>
+  <div class="text-center p-2">
+    <small>
+      Copy:
+      <CopyButton text={markdown}>Markdown Link</CopyButton>
+    </small>
+  </div>
 {/key}
