684f850ac13f42816b1ea6b3eab844d719929d52
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-10-04 15:29:10 +0000

    Fixed url to be reactive

--- packages/viewer/src/components/Shared/Cite.svelte
+++ packages/viewer/src/components/Shared/Cite.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
+  import { page } from '$app/stores'
   import CopyButton from './CopyButton.svelte'
-  let url = ''
-  let markdown = ''
   export let title: string =
     'π-Base, a community database of topological counterexamples.'
-  onMount(() => {
-    url = window.location.href
-    markdown = `[${title}](${url})`
-  })
+  $: markdown = `[${title}](${$page.url})`
 </script>
 
-{#key title}
-  <div class="text-center p-2">
-    <small>
-      Cite as:
-      <span class="text-muted">
-        The pi-Base Community.
-        <cite>{title}.</cite>
-        Available at: {url}
-        (Accessed: {new Date().toISOString().split('T')[0]}).
-      </span>
-    </small>
-  </div>
-  <div class="text-center p-2">
-    <small>
-      Copy:
-      <CopyButton text={markdown}>Markdown Link</CopyButton>
-    </small>
-  </div>
-{/key}
+<div class="text-center p-2">
+  <small>
+    Cite as:
+    <span class="text-muted">
+      The pi-Base Community.
+      <cite>{title}.</cite>
+      Available at: {$page.url}
+      (Accessed: {new Date().toISOString().split('T')[0]}).
+    </span>
+  </small>
+</div>
+<div class="text-center p-2">
+  <small>
+    Copy:
+    <CopyButton text={markdown}>Markdown Link</CopyButton>
+  </small>
+</div>
