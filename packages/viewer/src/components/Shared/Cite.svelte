695843956b075b21bc08c96506911f4673dd2df6
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-10-04 10:41:36 -0500

    Fix citation button on Explore page (#170)
    * Added key to markdown
    
    * lint
    
    * Updated key to remount the title
    
    * lint
    
    * Fixed url to be reactive

--- packages/viewer/src/components/Shared/Cite.svelte
+++ packages/viewer/src/components/Shared/Cite.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
+  import { page } from '$app/stores'
   import CopyButton from './CopyButton.svelte'
-  let url = ''
-  let markdown = ''
   export let title: string =
     'π-Base, a community database of topological counterexamples.'
-  onMount(() => {
-    url = window.location.href
-    markdown = `[${title}](${url})`
-  })
+  $: markdown = `[${title}](${$page.url})`
 </script>
 
 <div class="text-center p-2">
@@ -17,7 +12,7 @@
     <span class="text-muted">
       The pi-Base Community.
       <cite>{title}.</cite>
-      Available at: {url}
+      Available at: {$page.url}
       (Accessed: {new Date().toISOString().split('T')[0]}).
     </span>
   </small>
