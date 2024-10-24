47748c61a8e2cf2225a13291515bca2c9ff0066d
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-10-24 19:16:48 +0000

    Double curly braces markdown

--- packages/viewer/src/components/Shared/SeMarkdown.svelte
+++ packages/viewer/src/components/Shared/SeMarkdown.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
+  import { Ref } from "@pi-base/core";
+  
   export let source: string
   const modify = (s: string) => {
     const singlecurly = /{(?<type>[SPT])(?<id>\d+)}/g
-    const doublecurlies = /{{(?<prefix>\w)+:(?<id>[^{])+}}/g
-
-    // Replace single curly braces
-    s = s.replace(singlecurly, (type, id) => {
-      // Perform replacement based on type and id
-      return `${type}, ${id}`
-    })
+    const doublecurlies = /{{(?<prefix>\w+):(?<id>[^{]+)}}/g
 
     // Replace double curly braces
-    s = s.replace(doublecurlies, (prefix, id) => {
+    s = s.replace(doublecurlies, (_, prefix, id) => {
       // Perform replacement based on prefix and id
-      return `${prefix}, ${id}`
+      const ref = Ref.format({
+        "kind": prefix,
+        "id": id,
+      })
+      return `[${ref.title}](${ref.href})`
+
     })
+
+    // Replace single curly braces
+    /*s = s.replace(singlecurly, (type, id) => {
+      // Perform replacement based on type and id
+      return `${type}, ${id}`
+    })*/
+
+    
 
     return s
   }
