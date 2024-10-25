68afa7f1ce86f52d86acddeec677ce31ce31aea5
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-10-25 15:45:35 +0000

    Lint  + hacky solution to the single surly braces

--- packages/viewer/src/components/Shared/SeMarkdown.svelte
+++ packages/viewer/src/components/Shared/SeMarkdown.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { Ref } from "@pi-base/core";
-  
+  import { Ref } from '@pi-base/core'
+
   export let source: string
   const modify = (s: string) => {
     const singlecurly = /{(?<type>[SPT])(?<id>\d+)}/g
@@ -10,20 +10,23 @@
     s = s.replace(doublecurlies, (_, prefix, id) => {
       // Perform replacement based on prefix and id
       const ref = Ref.format({
-        "kind": prefix,
-        "id": id,
+        kind: prefix,
+        id: id,
       })
       return `[${ref.title}](${ref.href})`
-
     })
 
     // Replace single curly braces
-    /*s = s.replace(singlecurly, (type, id) => {
+    /*s = s.replace(singlecurly, (_, type, id) => {
       // Perform replacement based on type and id
-      return `${type}, ${id}`
+      if (type.equals("S")) {
+        return `[https://topology.pi-base.org/spaces/${type}${id}]()`
+      } else if (type.equals("P")) {
+        return `[https://topology.pi-base.org/properties/${type}${id}]()`
+      } else {
+        return `[https://topology.pi-base.org/theorems/${type}${id}]()`
+      }
     })*/
-
-    
 
     return s
   }
