1842b6bd293e87d8efbd5bdc2b4f8f61268de7ea
Steven Clontz <steven.clontz@gmail.com>
2024-11-02 16:28:51 +0000

    add aliases to other tables

---
+++ packages/viewer/src/components/Properties/Cell.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { Property } from '@/models'
+    import { Link } from '../Shared'
+    import Typeset from '../Shared/Typeset.svelte'
+    export let property: Property
+</script>
+
+<div><Link.Property {property} /></div>
+{#if property.aliases.length > 0}
+  <div>
+    <small class="text-muted">
+      {#each property.aliases as alias, i}
+        {#if i > 0}, {/if}
+        <Typeset body={alias} />
+      {/each}
+    </small>
+  </div>
+{/if}
