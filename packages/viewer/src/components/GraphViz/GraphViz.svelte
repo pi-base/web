9be5ef25bdd381c0012de16fbf28609e9728e6a9
Steven Clontz <steven.clontz@gmail.com>
2024-11-04 22:31:58 +0000

    add graphviz sandbox route

--- packages/viewer/src/components/GraphViz/GraphViz.svelte
+++ packages/viewer/src/components/GraphViz/GraphViz.svelte
@@ -4,10 +4,11 @@
   import * as d3 from 'd3'
   import * as graphviz from 'd3-graphviz'
   export let dot = 'digraph  {a -> b}'
-  let graphDiv : HTMLElement
+  let graphDiv: HTMLElement
   onMount(() => {
     d3.select(graphDiv).graphviz().renderDot(dot)
   })
+  $: if (graphDiv) { d3.select(graphDiv).graphviz().renderDot(dot) }
 </script>
 
 <div bind:this={graphDiv} style="text-align: center;" />
