d172e04694d007152aa5ac7e2b4acc3d1a720644
Steven Clontz <steven.clontz@gmail.com>
2024-11-04 22:27:32 +0000

    add GraphViz component

---
+++ packages/viewer/src/components/GraphViz/GraphViz.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  // @ts-nocheck
+  import { onMount } from 'svelte'
+  import * as d3 from 'd3'
+  import * as graphviz from 'd3-graphviz'
+  export let dot = 'digraph  {a -> b}'
+  let graphDiv : HTMLElement
+  onMount(() => {
+    d3.select(graphDiv).graphviz().renderDot(dot)
+  })
+</script>
+
+<div bind:this={graphDiv} style="text-align: center;" />
---
+++ packages/viewer/src/components/GraphViz/index.ts
@@ -0,0 +1,1 @@
+export { default as GraphViz } from './GraphViz.svelte'
