0f699b522fb10bf257484ae4b6eca20eea827955
abram! <115729028+Not-Abram@users.noreply.github.com>
2024-11-18 17:07:13 +0000

    Added support for traits

--- packages/viewer/src/components/Shared/ExtMarkdown.svelte
+++ packages/viewer/src/components/Shared/ExtMarkdown.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import { Ref } from '@pi-base/core'
   import context from '@/context'
-  const { spaces, theorems, properties } = context()
+  const { spaces, theorems, properties, traits } = context()
 
   export let source: string
   const modify = (s: string) => {
-    const singlecurly = /{(?<type>[SPT])(?<id>\d+)}/g
-    const doublecurlies = /{{(?<prefix>\w+):(?<id>[^{]+)}}/g
+    const internal_trait = /{S(?<spaceID>\d+)\|P(?<propID>\d+)}/g
+    const internal_SPT = /{(?<type>[SPT])(?<id>\d+)}/g
+    const external = /{{(?<prefix>\w+):(?<id>[^{]+)}}/g
 
     // Replace double curly braces
-    s = s.replace(doublecurlies, (_, prefix, id) => {
+    s = s.replace(external, (_, prefix, id) => {
       // Perform replacement based on prefix and id
       const ref = Ref.format({
         kind: prefix,
@@ -18,8 +19,22 @@
       return `[${ref.title}](${ref.href})`
     })
 
-    // Replace single curly braces
-    s = s.replace(singlecurly, (_, type, id) => {
+    // Replace single curly braces - check traits, then space/property/theorem
+    // Reuses logic from https://github.com/pi-base/web/blob/0c340a5e5c400047acd21acc1ea7a3f75c06733d/packages/viewer/src/parser/internalLinks.ts
+    // TODO: replace duplicate code
+    s = s.replace(internal_trait, (_, sid, pid) => {
+      const space = $spaces.find(Number(sid))
+      const property = $properties.find(Number(pid))
+      const trait = space && property && $traits.find(space, property)
+      const connector =
+        trait?.value === true ? 'is' : trait?.value === false ? 'is not' : '?'
+      return `[S${sid}|P${pid}: ${
+        space ? space.name : 'S' + sid
+      } ${connector} ${
+        property ? property.name : 'P' + pid
+      }](https://topology.pi-base.org/spaces/S${sid}/properties/P${pid})`
+    })
+    s = s.replace(internal_SPT, (_, type, id) => {
       // Perform replacement based on type and id
 
       if (type === 'S') {
