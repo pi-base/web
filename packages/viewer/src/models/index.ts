d9910cfb95248d4ebed883cd3a2e711b8f4958b5
Steven Clontz <steven.clontz@gmail.com>
2024-11-26 20:20:29 -0600

    Remove redundant import (#196)

--- packages/viewer/src/models/index.ts
+++ packages/viewer/src/models/index.ts
@@ -8,9 +8,8 @@ export { default as Theorems } from './Theorems'
 export { default as Traits } from './Traits'
 
 import type { Property as P, Trait as Tr } from '@/types'
-import type { default as Th } from './Theorem'
 
 export type Proof = {
   traits: [P, Tr][]
-  theorems: Th[]
+  theorems: Theorem[]
 }
