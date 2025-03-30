da85ae28512f624ac29da8cde344140dc762b3ab
James Dabbs <james.dabbs@gmail.com>
2025-03-30 13:03:58 -0700

    try out honeycomb frontend monitoring

--- packages/viewer/src/routes/+layout.ts
+++ packages/viewer/src/routes/+layout.ts
@@ -3,9 +3,33 @@ import { defaultHost } from '@/constants'
 import * as errors from '@/errors'
 import { sync } from '@/gateway'
 import type { LayoutLoad } from './$types'
+import { browser } from '$app/environment'
+
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web'
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web'
 
 export const load: LayoutLoad = async ({ fetch, url: { host } }) => {
   const dev = host.match(/(dev(elopment)?[.-]|localhost)/) !== null
+
+  if (browser) {
+    const configDefaults = {
+      ignoreNetworkEvents: true,
+    }
+
+    const sdk = new HoneycombWebSDK({
+      debug: dev,
+      apiKey: 'Sx608N12wwH9ZJ6qmHMVnA',
+      serviceName: 'π-base web',
+      instrumentations: [
+        getWebAutoInstrumentations({
+          '@opentelemetry/instrumentation-xml-http-request': configDefaults,
+          '@opentelemetry/instrumentation-fetch': configDefaults,
+          '@opentelemetry/instrumentation-document-load': configDefaults,
+        }),
+      ],
+    })
+    sdk.start()
+  }
 
   const errorHandler = dev
     ? errors.log()
