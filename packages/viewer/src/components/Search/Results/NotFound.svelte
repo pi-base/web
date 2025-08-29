f18221a4fcac1f506b58f5d588bccf3cc391a5fe
Steven Clontz <steven.clontz@gmail.com>
2025-08-29 07:41:10 -0500

    add prompt to contribute for empty search

--- packages/viewer/src/components/Search/Results/NotFound.svelte
+++ packages/viewer/src/components/Search/Results/NotFound.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import context from '@/context'
+  import { contributingUrl } from '@/constants'
   import { Formula } from '@/components/Shared'
   import type { Formula as F, Property } from '@/models'
   import { disprove } from '@/stores/deduction'
@@ -16,11 +17,20 @@
 {#if formula && proof}
   <Disprovable {formula} {proof} />
 {:else}
-  No spaces found
-  {#if text}matching <code>{text}</code>{/if}
-  {#if text && formula}and{/if}
-  {#if formula}
-    satisfying
-    <Formula value={formula} />
+  <p>
+    No spaces found
+    {#if text}matching <code>{text}</code>{/if}
+    {#if text && formula}and{/if}
+    {#if formula}
+      satisfying
+      <Formula value={formula} />.
+    {/if}
+  </p>
+  {#if !text}
+    <p>
+      <a href={contributingUrl}>
+        Contribute an example or theorem that answers this search.
+      </a>
+    </p>
   {/if}
 {/if}
