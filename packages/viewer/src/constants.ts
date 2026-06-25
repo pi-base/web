2dbf57071e022a1df429ebdecb04f3c56a6f7a2c
Steven Clontz <steven.clontz@gmail.com>
2026-06-25 08:08:03 -0500

    stub out customization

--- packages/viewer/src/constants.ts
+++ packages/viewer/src/constants.ts
@@ -4,7 +4,59 @@ const {
   VITE_BRANCH = 'unknown',
   VITE_MAIN_BRANCH = 'main',
   VITE_VERSION = 'unknown',
+  VITE_CATEGORY = 'topology'
 } = import.meta.env
+
+interface CategoryConfig {
+  category: string
+  objects: string
+  dataHost: string
+  contributingUrl: string
+  helpUrl: string
+  calloutFeatures: string[]
+  homeContent: string
+  showQuestions: boolean
+  acknowledgement?: {
+    quote: string
+    footer: string
+    dedication: string
+  }
+}
+
+const topologyConfig:CategoryConfig = {
+  category: "topology",
+  objects: "Spaces",
+  dataHost: "https://pub-65041ca69d744da88ade13abd31ad834.r2.dev",
+  contributingUrl: "https://code4math.zulipchat.com/#narrow/channel/416467-pi-base/topic/Welcome.20new.20and.20potential.20contributors!.20.F0.9F.91.8B/with/544214230",
+  helpUrl: "https://github.com/pi-base/data/wiki/",
+  calloutFeatures: [
+    "Search spaces by name/description: [compactifications](https://topology.pi-base.org/spaces?text=compactification)",
+    "Search spaces by properties: [non-metric continua](https://topology.pi-base.org/spaces?q=compact%20%2B%20connected%20%2B%20t_2%20%2B%20~metrizable)",
+    "Find counterexamples: [connected spaces need not be path connected](https://topology.pi-base.org/theorems/T000040)",
+  ],
+  homeContent: "# Contributing ...",
+  showQuestions: true,
+  acknowledgement: {
+    quote: "Topology is a dense forest of counterexamples. A usable map of the forest is a fine thing.",
+    footer: "Paraphrased from Mary Ellen Rudin's review of *Counterexamples in Topology*",
+    dedication: "Dedicated to the memory of our friend and mentor Gary Gruenhage (1947-2023)."
+  },
+}
+
+const graphConfig:CategoryConfig = {
+  category: "graph",
+  objects: "Graphs",
+  dataHost: "TODO",
+  contributingUrl: "TODO",
+  helpUrl: "https://github.com/pi-base/data-graph/wiki/",
+  calloutFeatures: [
+    "graphs are neat"
+  ],
+  homeContent: "# Contributing ...",
+  showQuestions: false,
+}
+
+export const categoryConfig = VITE_CATEGORY === "graph" ? graphConfig : topologyConfig
 
 export const mainBranch = VITE_MAIN_BRANCH
 export const defaultHost = VITE_BUNDLE_HOST
